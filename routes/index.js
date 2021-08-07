dc45c499019b2db8954d3090866bb24b4d708d48
Evan Furniss <evanfurniss@gmail.com>
2021-08-06 20:30:17 -0500

    Added Firebase information to initialize firebase, set up server and initial routes

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var path = require("path");
+var apiRoutes = require("./api");
+var app = require("express").Router();
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use("/api", apiRoutes);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const express = require("express");
 const routes = require("./routes");
 const app = express();
 const firebase = require("firebase/app");
+const admin = require("firebase-admin");
+const serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dnd-character-tracker-6b132-default-rtdb.firebaseio.com"
+});
