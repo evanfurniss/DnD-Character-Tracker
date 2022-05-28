fdae89780799c750a5bdc94d7c64e90c566c838e
evanfurniss <evanfurniss@gmail.com>
2022-05-28 14:14:50 -0500

    Adds controller functions for the character builder

--- controllers/charactersController.js
+++ controllers/charactersController.js
@@ -5,10 +5,32 @@ const charactersRef = db.collection("characters");
 
 module.exports = {
     addCharacter: function(req, res) {
+        try {
+            const doc = await charactersRef.add(req.body);
 
+            if (doc.exists) {
+                res.json({added: true});
+            } else {
+                res.json({added: false});
+            };
+        } catch (err) {
+            console.log(err);
+            res.json({message: "Could not add Character"});
+        };
     },
 
     getCharacters: function(req, res) {
-        
+        try {
+            const doc = await charactersRef.get();
+
+            if (doc.exists) {
+                res.json(doc.data());
+            } else {
+                res.json({users: "none"});
+            };
+        } catch (err) {
+            console.log(err);
+            res.json({message: "Could not get Characters"});
+        };
     }
 };
